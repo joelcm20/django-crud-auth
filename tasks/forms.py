54bc80da98eb5347974247553061ee9d0485ff1a
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-07-02 15:26:39 -0300

    forms y create_task: atributos y estilos agregados

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,14 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["title", "description", "important", "completed"]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control", "placeholder": "Write a title", "autofocus": ""}),
+            "description": forms.Textarea(attrs={"class": "form-control", "placeholder": "Write a description"}),
+            "important": forms.CheckboxInput(attrs={"class": "my-2 form-check-input"}),
+            "completed": forms.CheckboxInput(attrs={"class": "my-2 form-check-input"})
+        }
